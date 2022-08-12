ff48b736c266811cf97e3bf7d191271b3b3ee315
Tijil Chhabra <60564583+tijilchhabra1729@users.noreply.github.com>
2022-08-12 14:22:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from Tool import app
+import os
+from Tool.forms import RegistrationForm, LoginForm, SearchForm
+from Tool.models import User, Design, Adjective
+from flask import render_template, request, url_for, redirect, flash, abort
+from flask_login import current_user, login_required, login_user, logout_user
+from sqlalchemy import desc, asc
+from werkzeug.utils import secure_filename
+import stripe
+
+public_key = 'pk_test_6pRNASCoBOKtIshFeQd4XMUh'
+
+stripe.api_key = "sk_test_BQokikJOvBiI2HlWgH4olfQ2"
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template("index.htm")
